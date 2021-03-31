178f0598313ff549b888415723f1690347b87ecb
szk2s <suzukisjss@gmail.com>
2021-03-31 16:39:36 +0900

    Revert "add constexpr support to MathFunctions lib"
    This reverts commit 2541622401867a0d1eb3711552173ec8f5067d15.

--- MathFunctions/MathFunctions.h
+++ MathFunctions/MathFunctions.h
@@ -1,18 +1,1 @@
-inline constexpr double mysqrt(double x) {
-  if (x <= 0) {
-    return 0;
-  }
-
-  double result = x;
-
-  // do ten iterations
-  for (int i = 0; i < 10; ++i) {
-    if (result <= 0) {
-      result = 0.1;
-    }
-    double delta = x - (result * result);
-    result = result + 0.5 * delta / result;
-    // std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
-  }
-  return result;
-};
+double mysqrt(double x);
--- tutorial.cxx
+++ tutorial.cxx
@@ -7,26 +7,25 @@
 #include "MathFunctions.h"
 
 int main(int argc, char *argv[]) {
-  // if (argc < 2) {
-  //   // report version
-  //   std::cout << "Version " << TUTORIAL_VERSION_MAJOR << "."
-  //             << TUTORIAL_VERSION_MINOR << std::endl;
-  //   std::cout << "Usage: " << argv[0] << " number" << std::endl;
+  if (argc < 2) {
+    // report version
+    std::cout << "Version " << TUTORIAL_VERSION_MAJOR << "."
+              << TUTORIAL_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
 
-  //   const std::unordered_map<std::string, double> map{
-  //       {"a", 0.0}, {"b", 1.0}, {"c", 2.0}};
+    const std::unordered_map<std::string, double> map{
+        {"a", 0.0}, {"b", 1.0}, {"c", 2.0}};
         
-  //   // map.contains() is C++20 feature
-  //   std::cout << (map.contains("a") ? "true" : "false") << std::endl;
+    // map.contains() is C++20 feature
+    std::cout << (map.contains("a") ? "true" : "false") << std::endl;
 
-  //   return 1;
-  // }
+    return 1;
+  }
 
   // convert input to double
-  constexpr double inputValue = 9;
+  const double inputValue = std::stod(argv[1]);
   // calculate square root
-  constexpr double outputValue = mysqrt(inputValue);
-  static_assert(outputValue == 3);
+  const double outputValue = mysqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
 
