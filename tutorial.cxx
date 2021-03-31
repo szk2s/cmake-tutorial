2639f2f08dd3df91703909da99f8080184d4b970
szk2s <suzukisjss@gmail.com>
2021-03-31 13:21:55 +0900

    add version number and configured header file

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,12 +1,15 @@
 // A simple program that computes the square root of a number
+#include "TutorialConfig.h"
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc < 2) {
+    // report version
+    std::cout << "Version " << TUTORIAL_VERSION_MAJOR << "."
+              << TUTORIAL_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
@@ -18,5 +21,6 @@ int main(int argc, char* argv[])
   const double outputValue = sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
+
   return 0;
 }
