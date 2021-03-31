2541622401867a0d1eb3711552173ec8f5067d15
szk2s <suzukisjss@gmail.com>
2021-03-31 16:39:28 +0900

    add constexpr support to MathFunctions lib

--- tutorial.cxx
+++ tutorial.cxx
@@ -7,25 +7,26 @@
 #include "MathFunctions.h"
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    // report version
-    std::cout << "Version " << TUTORIAL_VERSION_MAJOR << "."
-              << TUTORIAL_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+  // if (argc < 2) {
+  //   // report version
+  //   std::cout << "Version " << TUTORIAL_VERSION_MAJOR << "."
+  //             << TUTORIAL_VERSION_MINOR << std::endl;
+  //   std::cout << "Usage: " << argv[0] << " number" << std::endl;
 
-    const std::unordered_map<std::string, double> map{
-        {"a", 0.0}, {"b", 1.0}, {"c", 2.0}};
+  //   const std::unordered_map<std::string, double> map{
+  //       {"a", 0.0}, {"b", 1.0}, {"c", 2.0}};
         
-    // map.contains() is C++20 feature
-    std::cout << (map.contains("a") ? "true" : "false") << std::endl;
+  //   // map.contains() is C++20 feature
+  //   std::cout << (map.contains("a") ? "true" : "false") << std::endl;
 
-    return 1;
-  }
+  //   return 1;
+  // }
 
   // convert input to double
-  const double inputValue = std::stod(argv[1]);
+  constexpr double inputValue = 9;
   // calculate square root
-  const double outputValue = mysqrt(inputValue);
+  constexpr double outputValue = mysqrt(inputValue);
+  static_assert(outputValue == 3);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
 
