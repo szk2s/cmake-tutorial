94a2d498635c0cab32654ed4bf9bebbae26f365e
szk2s <suzukisjss@gmail.com>
2021-03-31 16:28:56 +0900

    add MathFunctions library

--- tutorial.cxx
+++ tutorial.cxx
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include "MathFunctions.h"
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]) {
   // convert input to double
   const double inputValue = std::stod(argv[1]);
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  const double outputValue = mysqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
 
